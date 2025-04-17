70d098ffac2525fd59c111848183d2350bbc95e7
upioneer <hgrand@outlook.com>
2025-04-16 21:22:01 -0500

    Update install_dotnet_legacy.ps1

--- install_dotnet_legacy.ps1
+++ install_dotnet_legacy.ps1
@@ -1,3 +1,10 @@
+<#
+Notes
+  .NET 3.5 installation requires an internet connection unless you use local media.
+  DISM will fail on Windows 10/11 if you can’t reach Windows Update and don't provide a source path.
+  For offline or secure environments, include the SXS folder from the ISO in your package or reference a network share.
+#>
+
 # installs all legacy .net features including v2.0 and v2.5
 dism /Online /Enable-Feature /FeatureName:NetFx3 /All
 
