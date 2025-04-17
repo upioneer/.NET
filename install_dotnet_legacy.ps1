7192ff772379842c8ce1af2c1c60296971631746
upioneer <hgrand@outlook.com>
2025-04-16 21:17:10 -0500

    Update install_dotnet_legacy.ps1

--- install_dotnet_legacy.ps1
+++ install_dotnet_legacy.ps1
@@ -1,2 +1,8 @@
 # installs all legacy .net features including v2.0 and v2.5
 dism /Online /Enable-Feature /FeatureName:NetFx3 /All
+
+# enable .NET framework 3.5 from windows features
+Enable-WindowsOptionalFeature -Online -FeatureName NetFx3 -All -NoRestart
+
+# install from mounted windows install media. use correct drive letter
+DISM /Online /Enable-Feature /FeatureName:NetFx3 /All /Source:D:\sources\sxs /LimitAccess
